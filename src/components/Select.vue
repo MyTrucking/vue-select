a3165d8a1facb425b7cc1743bfde84cd89636fc8
Sam B <sam.bennett@mytrucking.com>
2019-12-19 11:47:57 +1300

    Added Tag that displays for Non-Billable Items

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -3,51 +3,59 @@
 </style>
 
 <template>
-  <div :dir="dir" class="v-select" :class="stateClasses">
-    <div ref="toggle" @mousedown.prevent="toggleDropdown" class="vs__dropdown-toggle">
-
-      <div class="vs__selected-options" ref="selectedOptions">
-        <slot v-for="option in selectedValue"
-              name="selected-option-container"
-              :option="normalizeOptionForSlot(option)"
-              :deselect="deselect"
-              :multiple="multiple"
-              :disabled="disabled">
-          <span :key="getOptionKey(option)" class="vs__selected">
-            <slot name="selected-option" v-bind="normalizeOptionForSlot(option)">
-              {{ getOptionLabel(option) }}
-            </slot>
-            <button v-if="multiple" :disabled="disabled" @click="deselect(option)" type="button" class="vs__deselect" aria-label="Deselect option" ref="deselectButtons">
-              <component :is="childComponents.Deselect" />
-            </button>
-          </span>
-        </slot>
-
-        <slot name="search" v-bind="scope.search">
-          <input class="vs__search" v-bind="scope.search.attributes" v-on="scope.search.events">
-        </slot>
+  <div :dir="dir" class="v-select DropdownSelector" :class="stateClasses">
+    <div :class="[{'DropdownSelector--NonBillable': selectedValue[0] && selectedValue[0].isBillable === false}, 'input-group-sm']">
+      <div class="DropdownSelector__Tag">
+        <span class="Tag Tag--Info" title="Non Billable">NB</span>
       </div>
 
-      <div class="vs__actions" ref="actions">
-        <button
-          v-show="showClearButton"
-          :disabled="disabled"
-          @click="clearSelection"
-          type="button"
-          class="vs__clear"
-          title="Clear selection"
-          ref="clearButton"
-        >
-          <component :is="childComponents.Deselect" />
-        </button>
+      <div class="form-control DropdownSelector__Input">
+        <div ref="toggle" @mousedown.prevent="toggleDropdown" class="vs__dropdown-toggle">
 
-        <slot name="open-indicator" v-bind="scope.openIndicator">
-          <component :is="childComponents.OpenIndicator" v-if="!noDrop" v-bind="scope.openIndicator.attributes"/>
-        </slot>
+          <div class="vs__selected-options" ref="selectedOptions">
+            <slot v-for="option in selectedValue"
+                  name="selected-option-container"
+                  :option="normalizeOptionForSlot(option)"
+                  :deselect="deselect"
+                  :multiple="multiple"
+                  :disabled="disabled">
+              <span :key="getOptionKey(option)" class="vs__selected">
+                <slot name="selected-option" v-bind="normalizeOptionForSlot(option)">
+                  {{ getOptionLabel(option) }}
+                </slot>
+                <button v-if="multiple" :disabled="disabled" @click="deselect(option)" type="button" class="vs__deselect" aria-label="Deselect option" ref="deselectButtons">
+                  <component :is="childComponents.Deselect" />
+                </button>
+              </span>
+            </slot>
 
-        <slot name="spinner" v-bind="scope.spinner">
-          <div class="vs__spinner" v-show="mutableLoading">Loading...</div>
-        </slot>
+            <slot name="search" v-bind="scope.search">
+              <input class="vs__search" v-bind="scope.search.attributes" v-on="scope.search.events">
+            </slot>
+          </div>
+
+          <div class="vs__actions" ref="actions">
+            <button
+              v-show="showClearButton"
+              :disabled="disabled"
+              @click="clearSelection"
+              type="button"
+              class="vs__clear"
+              title="Clear selection"
+              ref="clearButton"
+            >
+              <component :is="childComponents.Deselect" />
+            </button>
+
+            <slot name="open-indicator" v-bind="scope.openIndicator">
+              <component :is="childComponents.OpenIndicator" v-if="!noDrop" v-bind="scope.openIndicator.attributes"/>
+            </slot>
+
+            <slot name="spinner" v-bind="scope.spinner">
+              <div class="vs__spinner" v-show="mutableLoading">Loading...</div>
+            </slot>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -59,12 +67,15 @@
             v-for="(option, index) in filteredOptions"
             :key="getOptionKey(option)"
             class="vs__dropdown-option"
-            :class="{ 'vs__dropdown-option--selected': isOptionSelected(option), 'vs__dropdown-option--highlight': index === typeAheadPointer, 'vs__dropdown-option--disabled': !selectable(option) }"
+            :class="{ 'vs__dropdown-option--selected': isOptionSelected(option), 'vs__dropdown-option--highlight': index === typeAheadPointer, 'vs__dropdown-option--disabled': !selectable(option), 'DropdownSelector--NonBillable': option.isBillable === false}"
             @mouseover="selectable(option) ? typeAheadPointer = index : null"
             @mousedown.prevent.stop="selectable(option) ? select(option) : null"
           >
             <slot name="option" v-bind="normalizeOptionForSlot(option)">
-              {{ getOptionLabel(option) }}
+              <div class="DropdownSelector__DropdownOption">
+                <div v-if="option.isBillable === false" class="DropdownSelector__DropdownOptionTag"><span class="Tag Tag--Info" title="Non Billable">NB</span></div>
+                <div class="DropdownSelector__DropdownOptionText">{{ getOptionLabel(option) }}</div>
+              </div>
             </slot>
           </li>
           <li v-if="!filteredOptions.length" class="vs__no-options" @mousedown.stop="">
@@ -81,6 +92,7 @@
   import typeAheadPointer from '../mixins/typeAheadPointer'
   import ajax from '../mixins/ajax'
   import childComponents from './childComponents';
+  import Tag from './Tag';
 
   /**
    * @name VueSelect
