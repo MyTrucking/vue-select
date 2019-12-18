c970c99c78e9725890812d6f7b5c5a80b41dbe14
Sam B <sam.bennett@mytrucking.com>
2019-12-19 11:36:25 +1300

    Added component containing styles for Tags

---
+++ src/components/Tag.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        name: "Tag"
+    }
+</script>
+
+<style>
+    .Tag {
+        display: inline;
+        padding: .3em .4em .4em .6em;
+        font-weight: 700;
+        line-height: 1;
+        font-size: 75%;
+        color: #fff;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: baseline;
+        border-radius: .25em;
+    }
+
+    .Tag--Info {
+        background-color: #FF6B00;
+    }
+</style>
